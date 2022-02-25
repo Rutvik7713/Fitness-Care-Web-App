f202e04c220cfc39ea41a9954ae291cf40089591
Rutvik Patel <55801889+Rutvik7713@users.noreply.github.com>
2022-02-25 21:31:03 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export class Footer extends Component {
+  render() {
+    return (
+      <footer></footer>
+    )
+  }
+}
+
+export default Footer;
